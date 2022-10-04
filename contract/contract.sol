9f1a0388f8b0326a64164114716f2db1fa97f2d3
Sotatek-QuangNguyen5 <quang.nguyen5@sotatek.com>
2022-10-04 17:56:34 +0700

    update contract

--- contract/contract.sol
+++ contract/contract.sol
@@ -17,9 +17,16 @@ contract MyContract {
         bool isCreator;
         bool isSubciber;
         bool isUnSubcirber;
+        bool refund;
         uint256 amount;
     }
 
+    struct Transaction {
+
+        address sender;
+        address receiver;
+        uint256 amount;
+    }
 
     constructor() {
 
@@ -47,6 +54,7 @@ contract MyContract {
         subcribers[msg.sender].isCreator = false;
         subcribers[msg.sender].isSubciber = true;
         subcribers[msg.sender].isUnSubcirber = false;
+        subcribers[msg.sender].refund = false;
         subcribers[msg.sender].addr = msg.sender;
         subcribers[msg.sender].amount = msg.value;
         arrSubcirbers.push(subcribers[msg.sender]);
@@ -62,11 +70,47 @@ contract MyContract {
             revert("You are already registered !!!");
         } else {
 
-            require(msg.value >= 1 finney, "You need to pay at least 1 finney to sign the contract !!!");
+            require(msg.value >= 0.0001 ether, "You need to pay at least 1 finney to sign the contract !!!");
             addSubciber();
             return "You subcriber successfully !!!";
         }
     }
 
-    function unSubcribe() public return(string memory)
+    function removeElement(uint index) private {
+
+        if (index >= arrSubcirbers.length) {
+
+            revert("Error subcriber is not survive !!!");
+        }
+        for (uint i = index; i < arrSubcirbers.length - 1; i++ ) {
+
+            arrSubcirbers[i] = arrSubcirbers[i + 1];
+        }
+        arrSubcirbers.pop();
+    }
+
+    function unSubcribe() public Authentication returns(string memory) {
+
+        subcribers[msg.sender].isUnSubcirber = true;
+        for (uint i = 0; i < arrSubcirbers.length; i++) {
+
+            if (msg.sender == arrSubcirbers[i].addr) {
+
+                removeElement(i);
+                break;
+            }
+        }
+        return "You unsubcriber successfully !!!";
+    }
+
+    function Refund() public returns(string memory) {
+
+        require(subcribers[msg.sender].isSubciber == true  
+                && subcribers[msg.sender].isUnSubcirber == true
+                && subcribers[msg.sender].refund == false);
+        msg.sender.call{value: 0.0001 ether, gas: 5000};
+        subcribers[msg.sender].refund = true;
+        return "Refund successfully !!!";
+    }
+
 }
